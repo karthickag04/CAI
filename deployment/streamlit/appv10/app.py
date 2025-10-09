d365d7346d632c78fe1975f60250b4a3fd15d75e
karthickag04 <karthickag04@gmail.com>
2025-10-09 12:11:49 +0530

    added session file on conversation on streamlit usage

---
+++ deployment/streamlit/appv10/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.title("🎨 Matplotlib Chart Example")
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title("Sine Wave")
+
+st.pyplot(fig)
